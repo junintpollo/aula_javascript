a46a228bf12f9faa0e4ca8cd4b9372b349d6f473
Mauricio <juniomauricio367@yahoo.com>
2021-08-16 16:48:26 -0300

    versao0.1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Numero var idade = 29; 
+//String var idade = "29";
+
+var nome = "Mauricio Junio";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+//toLowerCase - Minúsculo 
+//toUpperCase - Maiúsculo 
+
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"))
